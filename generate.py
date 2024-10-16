6a5077129240679823f48b85f02795637b01e564
Jun Zhang <jun@junz.org>
2024-10-16 10:41:54 +0800

    Try to make the image look better

--- generate.py
+++ generate.py
@@ -4,8 +4,33 @@ from lxml import etree
 URL = "https://apod.nasa.gov/apod/"
 
 README = """
-<p align="center"><img src="{image_url}" width="500" height="500"></p>
-<h2 align="center">{image_desc}</h2>
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Astronomy Picture of the Day</title>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600&display=swap" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
+    <style>
+        body {
+            font-family: 'Poppins', sans-serif;
+        }
+    </style>
+</head>
+
+<body class="flex items-center justify-center h-screen bg-gray-100">
+
+    <div class="text-center">
+        <img src="{image_url}"
+            class="w-4/5 max-w-md mx-auto mb-4 rounded-lg  shadow-xl">
+        <p class="text-gray-700 font-semibold text-lg">{image_desc}</p>
+    </div>
+
+</body>
+
+</html>
 """
 
 
@@ -17,7 +42,8 @@ def fetch_image(base_url):
     html = etree.HTML(response.content)
 
     img_relative_url = html.xpath("//center//a/img/@src")
-    assert len(img_relative_url) == 1, "Mutiple images found!"
+    if len(img_relative_url) != 1:
+        return None, None
     image_url = base_url + img_relative_url[0]
 
     image_desc = html.xpath("//body//center/b/text()")[0]
@@ -32,5 +58,8 @@ def generate(readme_format, image_desc, image_url):
 
 if __name__ == "__main__":
     image_desc, image_url = fetch_image(URL)
+    if image_desc is None or image_url is None:
+        print("Invalid data when fetching the image, do nothing today")
+        return
     generate(README, image_desc, image_url)
 
