fc474201b8e8ee5aa5ff3de7d359538556c5d538
Jun Zhang <jun@junz.org>
2024-10-16 11:09:12 +0800

    css doens't work here, so sad

--- generate.py
+++ generate.py
@@ -4,33 +4,8 @@ from lxml import etree
 URL = "https://apod.nasa.gov/apod/"
 
 README = """
-<!DOCTYPE html>
-<html>
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Astronomy Picture of the Day</title>
-    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600&display=swap" rel="stylesheet">
-    <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
-    <style>
-        body {
-            font-family: 'Poppins', sans-serif;
-        }
-    </style>
-</head>
-
-<body class="flex items-center justify-center h-screen bg-gray-100">
-
-    <div class="text-center">
-        <img src="{image_url}"
-            class="w-4/5 max-w-md mx-auto mb-4 rounded-lg  shadow-xl">
-        <p class="text-gray-700 font-semibold text-lg">{image_desc}</p>
-    </div>
-
-</body>
-
-</html>
+<p align="center"><img src="{image_url}" width="500" height="500"></p>
+<h2 align="center">{image_desc}</h2>
 """
 
 
